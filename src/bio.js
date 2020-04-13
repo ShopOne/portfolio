5793758eeac7c966b1b54e0f57a10cbe44a90eed
ShopOne <oneky8080@gmail.com>
2020-04-13 22:09:43 +0900

    add link

--- src/bio.js
+++ src/bio.js
@@ -7,7 +7,7 @@ class BioList extends React.Component{
         return(<a id={elm.desc} href={elm.url}>{elm.text}</a>)
       };
       const timeStyle = {
-        animationDuration: ((idx+1)*1) + "s",
+        animationDuration: ((idx+1)*0.3) + "s",
       };
       var boxClassName = "bio-box bio-box-"+this.props.direction;
       if(elm.url != null) boxClassName += " valid-link";
@@ -33,6 +33,25 @@ class BioList extends React.Component{
     )
   }
 }
+class LinkList extends React.Component{
+  render(){
+    const links = otherLink.map((elm)=>{
+      return(
+        <li key={elm.desc} className="bio-otherlink-box">
+          <a 
+          href={elm.url}
+          className="bio-otherlink-text"
+          >{elm.desc}</a>
+        </li>
+      )
+    })
+    return(
+      <ul className="bio-otherlinks">
+      {links}
+      </ul>
+    )
+  }
+}
 export default class Biography extends React.Component{
   render(){
     const bioContents=[[],[]];
@@ -41,13 +60,18 @@ export default class Biography extends React.Component{
     })
     return(
       <div id="bio" align="center">
-        <BioList bios={bioContents[0]} direction="left"/>
-        <BioList bios={bioContents[1]} direction="right"/>
+        <h1 align="center" className="introduction">自己紹介</h1>
+        <div id="bio-lists">
+          <BioList bios={bioContents[0]} direction="left"/>
+          <BioList bios={bioContents[1]} direction="right"/>
+        </div>
+        <h1 className="introduction">Other Link</h1>
+        <LinkList />
       </div>
     )
   }
 }
-const bio =[
+const bio = [
   {desc:"Name",text:"Koki Yamashita",url:null},
   {desc:"Univ",text:"Meiji Univ.",url:null},
   {desc:"BirthDay",text:"1999/08/08",url:null},
@@ -57,4 +81,12 @@ const bio =[
   url:"https://play.google.com/store/apps/developer?id=shop_one"},
   {desc:"Qiita",text:"shop_one",url:"https://qiita.com/shop_one"},
   {desc:"Twitter",text:"@_shop_one",url:"https://twitter.com/_shop_one"},
+]
+const otherLink = [
+  {desc:"AtCoder Account",url:"https://atcoder.jp/users/shop_one"},
+  {desc:"Codeforces Account",url:"https://codeforces.com/profile/shop_one"},
+  {desc:"App(風来のシレンSFCサポーター)",
+    url:"https://play.google.com/store/apps/details?id=com.support.shirensupporter"},
+  {desc:"Game(ゆるふわSTG)",url:"https://www.freem.ne.jp/win/game/19515"},
+  {desc:"Game(hiyaya_ko)",url:"https://www.freem.ne.jp/win/game/22440"},
 ]
