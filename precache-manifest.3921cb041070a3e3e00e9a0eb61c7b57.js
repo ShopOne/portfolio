13680080a6fa4da09c0f2769a799db46f1d67918
ShopOne <oneky8080@gmail.com>
2020-04-14 00:30:08 +0900

    first build

---
+++ precache-manifest.3921cb041070a3e3e00e9a0eb61c7b57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e6b39d931198566a90a207dccfb14e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97b2f9c22e58fd146f9e",
+    "url": "/static/css/main.88947b43.chunk.css"
+  },
+  {
+    "revision": "4d1c00a9a7fb48a75e23",
+    "url": "/static/js/2.94388d84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.94388d84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97b2f9c22e58fd146f9e",
+    "url": "/static/js/main.5bb77a67.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  }
+]);
