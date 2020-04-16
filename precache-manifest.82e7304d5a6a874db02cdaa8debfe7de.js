6e61a2faf3b2b4458b8da69d3830774d5a6d302c
ShopOne <oneky8080@gmail.com>
2020-04-16 13:33:15 +0900

    version 1.0

---
+++ precache-manifest.82e7304d5a6a874db02cdaa8debfe7de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac7d8cf08f39e1842cf954bb6cdc7f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d31c39a995088c1305a",
+    "url": "/static/css/main.59f83101.chunk.css"
+  },
+  {
+    "revision": "2266ce3aa813405a7064",
+    "url": "/static/js/2.cc4b99b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cc4b99b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d31c39a995088c1305a",
+    "url": "/static/js/main.1b8d2698.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fd0e68cd0a94689a9f536ff6e1832623.js"
+  "/precache-manifest.82e7304d5a6a874db02cdaa8debfe7de.js"
 );
 
 self.addEventListener('message', (event) => {
