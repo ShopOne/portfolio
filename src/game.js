2969d21b8aeb0aa5047b3914548748f43a36c81c
ShopOne <oneky8080@gmail.com>
2020-04-22 16:20:12 +0900

    move bar!

--- src/game.js
+++ src/game.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import './game.css'
-class DrawHandler extends React.Component{
-  render(){
-    return(
-      <div id="Game-DrawHandler">
-        <div className="Game-ball"
-        style={{
-          left: this.props.ballPosX+"%",
-          bottom: this.props.ballPosY+"%",
-        }}
-        />
-      </div>
-    )
-  }
-}
+const MIN_X = 0,MAX_X = 700;
+const MIN_Y = 0,MAX_Y = 700;
+const BAR_MOVE_SPD = 5;
+const BALL_DEF_SPD = 5;
+const BALL_DIA = 30;
 function clampVal(x,low,high){
   if(x<low) return low;
   if(x>high) return high;
   return x;
+}
+class moveObject extends React.Component{
+}
+class DrawHandler extends React.Component{
+  render(){
+    const bars = this.props.bars.map((bar)=>{
+      return(
+        <div className="Game-bar"
+        key={bar.key}
+        style={{
+          left: bar.posX+"px",
+          top: bar.posY+"px",
+          width: bar.width+"px",
+          height: bar.height+"px",
+        }}
+        />
+      )
+    })
+    return(
+      <div id="Game-DrawHandler">
+        <div className="Game-ball"
+        style={{
+          left: this.props.ballPosX+"px",
+          bottom: this.props.ballPosY+"px",
+        }}
+        />
+        {bars}
+      </div>
+    )
+  }
 }
 export default class GameMain extends React.Component{
   constructor(props){
@@ -27,17 +48,91 @@ export default class GameMain extends React.Component{
       ballPosY: 20,
       ballVX: 0.3,
       ballVY: 1.0,
+      upKey: false,
+      downKey: false,
+      leftKey: false,
+      rightKey: false,
+      bars:[
+        {posX: 350,posY:  0,
+          width: 200,height:  20,key: "top"},
+        {posX: 350,posY: 680,
+          width: 200,height:  20,key: "bottom"},
+        {posX:  0,posY: 350,
+          width:  20,height: 200,key: "left"},
+        {posX: 680,posY: 350,
+          width:  20,height: 200,key: "right"},
+      ],
     }
     this.moveBall = this.moveBall.bind(this);
+    this.keyHandler = this.keyHandler.bind(this);
     this.timeID = setInterval(
-      () => this.moveBall(),
+      () => this.moveObject(),
       10
     )
   }
-  minX = 0;
-  maxX = 86;
-  minY = 0
-  maxY = 85;
+  setBar(bar,moveX,moveY){
+    bar.posX = clampVal(bar.posX+moveX,MIN_X,MAX_X-bar.width);
+    bar.posY = clampVal(bar.posY+moveY,MIN_Y,MAX_Y-bar.height);
+  }
+  moveBar(){
+    var topBar = this.state.bars[0];
+    var bottomBar = this.state.bars[1];
+    var leftBar = this.state.bars[2];
+    var rightBar = this.state.bars[3];
+    if(this.state.upKey){
+      this.setBar(leftBar,0,BAR_MOVE_SPD);
+      this.setBar(rightBar,0,-BAR_MOVE_SPD);
+    }
+    if(this.state.downKey){
+      this.setBar(leftBar,0,-BAR_MOVE_SPD);
+      this.setBar(rightBar,0,BAR_MOVE_SPD);
+    }
+    if(this.state.leftKey){
+      this.setBar(topBar,BAR_MOVE_SPD,0);
+      this.setBar(bottomBar,-BAR_MOVE_SPD,0);
+    }
+    if(this.state.rightKey){
+      this.setBar(topBar,-BAR_MOVE_SPD,0);
+      this.setBar(bottomBar,BAR_MOVE_SPD,0);
+    }
+    this.setState({
+      bars: [
+        topBar,bottomBar,leftBar,rightBar,
+      ]
+    })
+  }
+  keyHandler(e,isPushed){
+    var upKey = this.state.upKey;
+    var downKey = this.state.downKey;
+    var leftKey = this.state.leftKey;
+    var rightKey = this.state.rightKey;
+    switch(e.key){
+      case 'ArrowUp':
+        upKey = isPushed;
+        e.preventDefault();
+        break;
+      case 'ArrowDown':
+        downKey = isPushed;
+        e.preventDefault();
+        break;
+      case 'ArrowLeft':
+        leftKey = isPushed;
+        e.preventDefault();
+        break;
+      case 'ArrowRight':
+        rightKey = isPushed;
+        e.preventDefault();
+        break;
+      default:
+        break;
+    }
+    this.setState({
+      upKey: upKey,
+      downKey: downKey,
+      leftKey: leftKey,
+      rightKey: rightKey,
+    })
+  }
   moveBall(){
     var nowX = this.state.ballPosX;
     var nowY = this.state.ballPosY;
@@ -45,15 +140,15 @@ export default class GameMain extends React.Component{
     var vy = this.state.ballVY;
     var nextX = nowX + vx;
     var nextY = nowY + vy;
-    if(nextX < this.minX ||nextX > this.maxX){
+    if(nextX < MIN_X ||nextX > MAX_X){
       vx = -vx;
     }
 
-    if(nextY < this.minY ||nextY > this.maxY){
+    if(nextY < MIN_Y ||nextY > MAX_Y){
       vy = -vy;
     }
-    nextX = clampVal(nextX,this.minX,this.maxX);
-    nextY = clampVal(nextY,this.minY,this.maxY);
+    nextX = clampVal(nextX,MIN_X,MAX_X);
+    nextY = clampVal(nextY,MIN_Y,MAX_Y);
     this.setState({
       ballPosX: nextX,
       ballPosY: nextY,
@@ -61,12 +156,21 @@ export default class GameMain extends React.Component{
       ballVY: vy,
     })
   }
+  moveObject(){
+    // this.moveBall();
+    this.moveBar();
+  }
   render(){
     return(
-      <div id="Game">
+      <div id="Game"
+      onKeyDown={(e)=>this.keyHandler(e,true)}
+      onKeyUp={(e)=>this.keyHandler(e,false)}
+      tabIndex='0'
+      >
         <DrawHandler
         ballPosX={this.state.ballPosX}
         ballPosY={this.state.ballPosY}
+        bars={this.state.bars}
         />
       </div>
     )
