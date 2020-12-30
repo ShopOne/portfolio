525993dbed32d5c7c2e1bc75c1e011a728cbe750
ShopOne <oneky8080@gmail.com>
2020-12-31 01:55:13 +0900

    fix desc

--- src/introduction.js
+++ src/introduction.js
@@ -6,15 +6,14 @@ export default class Description extends React.Component{
       <div id="introduction">
         <h2>所属</h2>
         <p>明治大学理工学部情報科学科(B3)</p>
-        <p>Integrated Quality(情報処理サークル)幹事長</p>
+        <p>Integrated Quality(情報処理サークル)元幹事長</p>
         <h2>PR</h2>
         <p>競技プログラミングとスマホアプリ開発を中心に活動をしています。 <br/>
         また、低レイヤーにも興味があります。少し前は自作OSを試していました。<br/>
         </p>
         <h2>趣味</h2>
         <p>開発以外の趣味としてはゲーム、
-        特にローグライクゲームと縦シューティングが好きです。<br/>
-        また、パズルも好きでweb上で解いていたりニコリ社のパズルを買っています。</p>
+        特にローグライクゲームと縦シューティングが好きです。</p>
         <h2>実績</h2>
         <p>Comming soon...</p>
       </div>
